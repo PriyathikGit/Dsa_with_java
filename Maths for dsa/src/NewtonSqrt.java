a2ae73f7187cdcd7bd6d2e60206a082628829b44
Rohan <techhack237@gmail.com>
2024-04-07 12:14:58 +0530

    "maths for dsa code"

--- Maths for dsa/src/NewtonSqrt.java
+++ Maths for dsa/src/NewtonSqrt.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class NewtonSqrt {
+package PACKAGE_NAME;
+public class NewtonSqrt {
+    public static void main(String[] args) {
+        System.out.println(sqrt(40));
+    }
+    static double sqrt(double n) {
+        double x = n;
+        double root;
+        while (true) {
+            root = 0.5 * (x + (n/x));
+
+            if (Math.abs(root - x) < 0.5) {
+                break;
+            }
+
+            x = root;
+        }
+        return root;
+    }
 }
--- Recursion/src/Fibo.java
+++ Recursion/src/Fibo.java
@@ -5,12 +5,13 @@ public class Fibo {
 //        for (int i = 0; i < 50; i++) {
 //            System.out.println(fiboFormula(i));
 //        }
-        System.out.println(fiboFormula(50));
+        // System.out.println(fiboFormula(50));
+        System.out.println(fibo(4));
     }
 
-    static int fiboFormula(int n){
-        return (int)(Math.pow(((1+Math.sqrt(5)) / 2),n) / Math.sqrt(5));
-    }
+    // static int fiboFormula(int n){
+    //     return (int)(Math.pow(((1+Math.sqrt(5)) / 2),n) / Math.sqrt(5));
+    // }
      static int fibo(int n) {
         if(n<2){
             return n;
